afefa0af0cc994cb78cf1ca923ec2fba0f56f05b
Augusto Simionato <guto.baroni@gmail.com>
2025-08-16 15:29:06 -0300

    Initial Commit

---
+++ SimBuddy/SimBuddyApp.swift
@@ -0,0 +1,17 @@
+//
+//  SimBuddyApp.swift
+//  SimBuddy
+//
+//  Created by Augusto Simionato on 16/08/25.
+//
+
+import SwiftUI
+
+@main
+struct SimBuddyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
